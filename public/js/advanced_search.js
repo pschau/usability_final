dd89b1cea11faffac0711fc3df90f1ec61c6d2b4
Peter Chau <pschau@1x-vl929-72-19-103-193.wireless.umass.edu>
2015-12-13 22:48:25 -0500

    advanced search filtering options

--- public/js/advanced_search.js
+++ public/js/advanced_search.js
@@ -1,6 +1,13 @@
-$(function () {
-    $('#button').on('click', function () {
-        var text = $('#text');
-        text.val(text.val() + ' after clicking');    
+
+
+
+    $('#reload-ingred').click( function () {
+        $("label[for='text1']").text("");
+  		$("label[for='text2']").text("");
+  		$("label[for='text3']").text("");
+  		$("label[for='text4']").text("");
+  		$("label[for='text5']").text("");
+  		$("label[for='text6']").text("");
+  		$("label[for='text7']").text("");
+  		$("label[for='text8']").text("");
     });
-});
